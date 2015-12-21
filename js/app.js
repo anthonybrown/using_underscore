a7d1f757ef5cdac6dd24145cdc9753c1eebd4389
anthonybrown <tony@tonybrown.me>
2015-12-20 22:10:48 -0500

    initial commit on working with Arrays and Objects with underscore.js

---
+++ js/app.js
@@ -0,0 +1,42 @@
+var msgEach = document.getElementById('each');
+var objMsg = document.getElementById('obj');
+var people = document.getElementById('people');
+
+var names = ['Tony', 'Klaudia', 'Jacob', 'Zeu Zeu', 'Shaggy', 'Bunna', 'Stormy', 'Rosco', 'Monkey', 'Chinchilly'];
+
+var name = _.each(names, function (elem, index, list) {
+	msgEach.innerHTML +=
+		'Element: '   + elem  + ' '  +
+		'Index: '     + index + ', ' +
+		'List length' + list.length + '<br />'
+});
+
+var author = {
+	firstName: 'Tony',
+	lastName: 'Brown'
+}
+
+_.each(author, function (val, key) {
+	objMsg.innerHTML +=
+		'Value: ' + '<strong>' + val + '</strong>' + ', ' + '<br />' +
+		' Key: ' +  '<em><strong>' + key + '</em></strong>' + '<br />'
+})
+
+var peoples = {
+	names: ['Tony', 'Klaudia', 'Jacob'],
+	getMessage: function (name) {
+		return 'Hello there ' + name + '!' + '<br>'
+	}
+}
+
+_.each(peoples.names, function (elem, index, list) {
+	people.innerHTML += this.getMessage(elem)
+}, peoples);
+// passing in peoples after iterator
+// as the last argument to the each function
+// will bind peoples to the this pointer.
+// without passing in peoples
+// it would point to the iterator function
+// instead of the getMessage method
+// very handy to pass in the context to the each function.
+
