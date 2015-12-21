9a06a0595fbd1f2d0bbb991a2c56654c2ea9a3b8
anthonybrown <tony@tonybrown.me>
2015-12-21 05:19:28 -0500

    added jQuery

--- js/app.js
+++ js/app.js
@@ -1,14 +1,39 @@
 var msgEach = document.getElementById('each');
 var objMsg = document.getElementById('obj');
 var people = document.getElementById('people');
+var authors = document.getElementById('authors');
+var mapMsg = document.getElementById('mapMsg');
+
+// log function
+var log = function (contents) {
+	if (_.isArray(contents)) {
+		_.each(contents, function (e, i, l) {
+			log(e)
+		});
+	} else {
+		$('#result').append('<li>' + contents + '</li>');
+	}
+};
+
+var logAuth = function (contents) {
+	if (_.isArray(contents)) {
+		_.each(contents, function(e, i, l) {
+			logAuth(e);
+		});
+	} else {
+		$('#authors').append('<li>' + contents + '</li>');
+	}
+};
 
 var names = ['Tony', 'Klaudia', 'Jacob', 'Zeu Zeu', 'Shaggy', 'Bunna', 'Stormy', 'Rosco', 'Monkey', 'Chinchilly'];
 
 var name = _.each(names, function (elem, index, list) {
-	msgEach.innerHTML +=
+	//msgEach.innerHTML +=
+	$('#each').append(
 		'Element: '   + elem  + ' '  +
 		'Index: '     + index + ', ' +
 		'List length' + list.length + '<br />'
+	);
 });
 
 var author = {
@@ -17,13 +42,15 @@ var author = {
 }
 
 _.each(author, function (val, key) {
-	objMsg.innerHTML +=
+	//objMsg.innerHTML +=
+	$('#obj').append(
 		'Value: ' + '<strong>' + val + '</strong>' + ', ' + '<br />' +
 		' Key: ' +  '<em><strong>' + key + '</em></strong>' + '<br />'
+	)
 })
 
 var peoples = {
-	names: ['Tony', 'Klaudia', 'Jacob'],
+	names: ['Barak Obama', 'Hillary Clinton', 'Bill Clinton'],
 	getMessage: function (name) {
 		return 'Hello there ' + name + '!' + '<br>'
 	}
@@ -39,3 +66,28 @@ _.each(peoples.names, function (elem, index, list) {
 // passing in the context to the each function
 // is very handy
 
+
+var mapNames = ['Tony', 'Klaudia', 'Jacob'],
+		m = _.map(mapNames, function(name) {
+			return name + ' is mapped from an array.<br>'
+		})
+log(m);
+
+var auths = {
+	first: 'Ernest HemingWay',
+	second : 'Edgar Allen Poe'
+},
+
+messenger = {
+	getGreeting: function() {
+		return 'Hello! ';
+	},
+
+	getMessage: function ( msg ) {
+		return this.getGreeting() + msg;
+	}
+};
+
+_.each(auths, function (value, key) {
+	logAuth(this.getMessage(value));
+}, messenger);
